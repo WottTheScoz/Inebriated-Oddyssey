8762be43216e17ba09cf829a550fccffb0f9be0c
Brayan Soto <brayansoto413@gmail.com>
2024-02-23 21:15:25 -0500

    CharacterController

---
+++ Inebriated Oddyssey/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public int health = 3;
+    private bool isFacingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(x, 0, z).normalized;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if ((isFacingRight && x < 0) || (!isFacingRight && x > 0))
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}

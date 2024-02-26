1bac576fd50407f6b3c32993fca9a2760baf99b4
Bernard <bernardthebear19@gmail.com>
2024-02-25 19:36:47 -0500

    Spawpoints
    Made it so the obstacles spawn points are nonactve when the obstacles spawn in.

--- Inebriated Oddyssey/Assets/Scripts/Map.cs
+++ Inebriated Oddyssey/Assets/Scripts/Map.cs
@@ -6,15 +6,26 @@ public class Map : MonoBehaviour
 {
     public GameObject[] Objects;
     // Start is called before the first frame update
+    
+    //this is referring to the spawn-points that are set on each map.
+    private GameObject spawn;
     void Start()
     {
-        int rand =  Random.Range(0, Objects.Length);
-        Instantiate(Objects[rand], transform.position, Quaternion.identity);
+        Spawner();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Spawner()
+    {
+        int rand =  Random.Range(0, Objects.Length);
+        Instantiate(Objects[rand], transform.position, Quaternion.identity);
+        //here the spawnpoints will become unactive after they are spawned in by the function
+        spawn = GameObject.FindGameObjectWithTag("Spawnpoint");
+        spawn.SetActive(false);
     }
 }

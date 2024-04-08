c074ed42f95f94f393196987ba8378806bd3f734
Bernard <bernardthebear19@gmail.com>
2024-04-07 23:15:52 -0400

    MAP
    S

--- Inebriated Oddyssey/Assets/Scripts/MapManager.cs
+++ Inebriated Oddyssey/Assets/Scripts/MapManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,6 +15,9 @@ public class MapManager : MonoBehaviour
     private void Awake()
     {
         GM = Map.GetComponent<GameManager>();
+        writeTXT();
+        writeXML();
+        writeJson();
     }
 
     // Start is called before the first frame update
@@ -43,6 +48,57 @@ public class MapManager : MonoBehaviour
         else
         {
             SceneManager.LoadScene("Level 1");
+        }
+    }
+    
+    public void writeXML() {
+        string xmlPath = Directory.GetCurrentDirectory() + @"\Assets\SaveData\" + "MapXML.xml";
+    
+        if (!File.Exists(xmlPath))
+        {
+            using (FileStream xmlStream = File.Create(xmlPath))
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(xmlPath))
+                { 
+                    xmlWriter.WriteStartElement("Levels");
+                    xmlWriter.WriteStartElement("Level 1");
+                    xmlWriter.WriteEndElement();
+                    xmlWriter.Close();
+                }
+            }
+        }
+    
+    }
+            
+    public void writeJson()
+    {
+        string jsonPath = Directory.GetCurrentDirectory() + @"\Assets\SaveData\" + "MapJSON.json";
+        if (!File.Exists(jsonPath))
+        {
+            using (FileStream jsonStream = File.Create(jsonPath))
+            {
+                JsonUtility.ToJson("Welcome to Json");
+            }
+        }
+    }
+    
+    public void writeTXT()
+    {
+        string path = Directory.GetCurrentDirectory() + @"\Assets\SaveData\" + "MapTXT.txt";
+                
+        if (!File.Exists(path))
+        {
+            using (StreamWriter txt = File.CreateText(path))
+            {
+                txt.WriteLine("This is from the Map Manager");
+            }
+        }
+        else
+        {
+            using (StreamWriter txt = File.AppendText(path))
+            {
+                txt.WriteLine("This is from the Map Manager");
+            }
         }
     }
 }

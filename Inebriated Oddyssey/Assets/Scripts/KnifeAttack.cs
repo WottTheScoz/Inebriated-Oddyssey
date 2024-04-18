ec1e54e02c5066d70ec0611e95224a144a4d6383
rickyje3 <rickyjelia@gmail.com>
2024-04-18 14:47:39 -0400

    Moved some stuff around but nothing important

--- Inebriated Oddyssey/Assets/Scripts/KnifeAttack.cs
+++ Inebriated Oddyssey/Assets/Scripts/KnifeAttack.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class KnifeAttack : MonoBehaviour
 {
-    float attackTime = 3f;
+    float attackTime = 2.5f;
     float timer;
 
     [SerializeField] GameObject sliceLeft;
